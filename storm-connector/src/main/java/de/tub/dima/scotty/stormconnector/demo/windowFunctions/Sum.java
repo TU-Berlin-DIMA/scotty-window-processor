1581aab0eb532bc326c6de36e3ce33f416d903c6
Batuhan Tüter <tuterbatuhan@gmail.com>
2019-09-13 10:44:42 +0200

    Scotty Connector for Apache Storm (#10)
    Scotty Connector for Apache Storm

---
+++ storm-connector/src/main/java/de/tub/dima/scotty/stormconnector/demo/windowFunctions/Sum.java
@@ -0,0 +1,15 @@
+package de.tub.dima.scotty.stormconnector.demo.windowFunctions;
+
+import de.tub.dima.scotty.core.windowFunction.InvertibleReduceAggregateFunction;
+
+public class Sum implements InvertibleReduceAggregateFunction<Integer> {
+    @Override
+    public Integer invert(Integer currentAggregate, Integer toRemove) {
+        return currentAggregate - toRemove;
+    }
+
+    @Override
+    public Integer combine(Integer partialAggregate1, Integer partialAggregate2) {
+        return partialAggregate1 + partialAggregate2;
+    }
+}
