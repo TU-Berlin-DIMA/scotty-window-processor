db696bec4a6441f736587f29ecf97e054fd1a2b9
Batuhan Tüter <tuterbatuhan@gmail.com>
2019-09-13 10:49:39 +0200

    Scotty Connector for Apache Beam (#11)
    Scotty Connector for Apache Beam

---
+++ beam-connector/src/main/java/de/tub/dima/scotty/beamconnector/demo/BeamSumDemo.java
@@ -0,0 +1,50 @@
+package de.tub.dima.scotty.beamconnector.demo;
+
+import de.tub.dima.scotty.beamconnector.KeyedScottyWindowOperator;
+import de.tub.dima.scotty.beamconnector.demo.dataGenerator.DataGeneratorSource;
+import de.tub.dima.scotty.beamconnector.demo.dataGenerator.TimeStampGenerator;
+import de.tub.dima.scotty.beamconnector.demo.windowFunctions.Mean;
+import de.tub.dima.scotty.beamconnector.demo.windowFunctions.Sum;
+import de.tub.dima.scotty.core.windowType.SessionWindow;
+import de.tub.dima.scotty.core.windowType.SlidingWindow;
+import de.tub.dima.scotty.core.windowType.TumblingWindow;
+import de.tub.dima.scotty.core.windowType.WindowMeasure;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.Read;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+
+public class BeamSumDemo {
+    public static void main(String[] args) {
+
+        PipelineOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().create();
+        Pipeline p = Pipeline.create(options);
+        System.out.println("Running Pipeline\n " + p.getOptions());
+
+        PCollection<KV<Integer, Integer>> data = p.begin().apply(Read.from(new DataGeneratorSource(0, new TimeStampGenerator())));
+
+        KeyedScottyWindowOperator<Integer, Integer> scottyWindowDoFn = new KeyedScottyWindowOperator<Integer, Integer>(0, new Sum());
+        scottyWindowDoFn.addWindow(new TumblingWindow(WindowMeasure.Time, 5000));
+        //scottyWindowDoFn.addWindow(new SlidingWindow(WindowMeasure.Time, 2000, 1000));
+        //scottyWindowDoFn.addWindow(new SessionWindow(WindowMeasure.Time, 2000));
+
+        //Apply Scotty Windowing
+        PCollection<String> result = data.apply(ParDo.of(scottyWindowDoFn));
+
+        //Print window results
+        result.apply(ParDo.of(new printObject()));
+        p.run().waitUntilFinish();
+
+    }
+
+    static class printObject extends DoFn<Object, String> {
+        @ProcessElement
+        public void processElement(@Element Object input) {
+            System.out.println(input.toString());
+        }
+    }
+}
---
+++ beam-connector/src/main/java/de/tub/dima/scotty/beamconnector/demo/windowFunctions/Count.java
@@ -0,0 +1,24 @@
+package de.tub.dima.scotty.beamconnector.demo.windowFunctions;
+
+import de.tub.dima.scotty.core.windowFunction.InvertibleReduceAggregateFunction;
+import org.apache.beam.sdk.values.KV;
+
+import java.io.Serializable;
+
+public class Count implements InvertibleReduceAggregateFunction<KV<Integer, Integer>>, Serializable {
+
+    @Override
+    public KV<Integer, Integer> lift(KV<Integer, Integer> inputTuple) {
+        return KV.of(inputTuple.getKey(), 1);
+    }
+
+    @Override
+    public KV<Integer, Integer> combine(KV<Integer, Integer> partialAggregate1, KV<Integer, Integer> partialAggregate2) {
+        return KV.of(partialAggregate1.getKey(), partialAggregate1.getValue() + partialAggregate2.getValue());
+    }
+
+    @Override
+    public KV<Integer, Integer> invert(KV<Integer, Integer> currentAggregate, KV<Integer, Integer> toRemove) {
+        return KV.of(currentAggregate.getKey(), currentAggregate.getValue() - toRemove.getValue());
+    }
+}
