bf8f7b61e39025c5768e7a85c26c3604f9962099
AndreejS <andrej.savinov@campus.tu-berlin.de>
2020-04-09 10:00:09 +0200

    Scotty connector for Apache Spark

---
+++ spark-connector/src/main/java/de/tub/dima/scotty/sparkconnector/demo/windowFunctions/MinWindowFunction.java
@@ -0,0 +1,10 @@
+package de.tub.dima.scotty.sparkconnector.demo.windowFunctions;
+
+import de.tub.dima.scotty.core.windowFunction.ReduceAggregateFunction;
+
+public class MinWindowFunction implements ReduceAggregateFunction<Integer> {
+    @Override
+    public Integer combine(Integer partialAggregate1, Integer partialAggregate2) {
+        return Math.min(partialAggregate1, partialAggregate2);
+    }
+}
