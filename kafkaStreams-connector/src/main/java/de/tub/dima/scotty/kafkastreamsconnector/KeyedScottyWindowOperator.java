8251fb462cce99e83a934fe6ccd2bbdfd5efb42d
AndreejS <39523653+AndreejS@users.noreply.github.com>
2020-08-13 12:59:29 +0200

    Scotty connector for Apache Kafka Streams (#25)

---
+++ kafkaStreams-connector/src/main/java/de/tub/dima/scotty/kafkastreamsconnector/KeyedScottyWindowOperator.java
@@ -0,0 +1,94 @@
+package de.tub.dima.scotty.kafkastreamsconnector;
+
+import de.tub.dima.scotty.core.AggregateWindow;
+import de.tub.dima.scotty.core.windowFunction.AggregateFunction;
+import de.tub.dima.scotty.core.windowType.SlidingWindow;
+import de.tub.dima.scotty.core.windowType.TumblingWindow;
+import de.tub.dima.scotty.core.windowType.Window;
+import de.tub.dima.scotty.slicing.SlicingWindowOperator;
+import de.tub.dima.scotty.state.memory.MemoryStateFactory;
+import org.apache.kafka.streams.processor.Processor;
+import org.apache.kafka.streams.processor.ProcessorContext;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class KeyedScottyWindowOperator<Key, Value> implements Processor<Key,Value> {
+    private ProcessorContext context;
+    private MemoryStateFactory stateFactory;
+    private HashMap<Key, SlicingWindowOperator<Value>> slicingWindowOperatorMap;
+    private long lastWatermark;
+    private final AggregateFunction windowFunction;
+    private final List<Window> windows;
+    private long allowedLateness;
+    private long watermarkEvictionPeriod = 100;
+
+    public KeyedScottyWindowOperator(AggregateFunction windowFunction, long allowedLateness) {
+        this.windowFunction = windowFunction;
+        this.windows = new ArrayList<>();
+        this.allowedLateness = allowedLateness;
+    }
+
+    @Override
+    public void init(ProcessorContext processorContext) {
+        this.context = processorContext;
+        this.stateFactory = new MemoryStateFactory();
+        this.slicingWindowOperatorMap = new HashMap<>();
+    }
+
+    @Override
+    public void process(Key currentKey, Value value) {
+        if (!slicingWindowOperatorMap.containsKey(currentKey)) {
+            slicingWindowOperatorMap.put(currentKey, initWindowOperator());
+        }
+        SlicingWindowOperator<Value> slicingWindowOperator = slicingWindowOperatorMap.get(currentKey);
+        slicingWindowOperator.processElement(value, context.timestamp());
+        processWatermark(context.timestamp(), currentKey);
+    }
+
+    public SlicingWindowOperator<Value> initWindowOperator() {
+        SlicingWindowOperator<Value> slicingWindowOperator = new SlicingWindowOperator<>(stateFactory);
+        for (Window window : windows) {
+            slicingWindowOperator.addWindowAssigner(window);
+        }
+        slicingWindowOperator.addAggregation(windowFunction);
+        slicingWindowOperator.setMaxLateness(allowedLateness);
+        return slicingWindowOperator;
+    }
+
+
+
+    private void processWatermark(long timeStamp, Key currentKey) {
+        if (timeStamp > lastWatermark + watermarkEvictionPeriod) {
+            for (SlicingWindowOperator<Value> slicingWindowOperator : this.slicingWindowOperatorMap.values()) {
+                List<AggregateWindow> aggregates = slicingWindowOperator.processWatermark(timeStamp);
+                for (AggregateWindow<Value> aggregateWindow : aggregates) {
+                    if (aggregateWindow.hasValue()) {
+                        System.out.println(aggregateWindow);
+                        for (Value aggValue : aggregateWindow.getAggValues()) {
+                            context.forward(currentKey, aggValue);
+                        }
+                    }
+                }
+            }
+            lastWatermark = timeStamp;
+        }
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    /**
+     * Register a new @{@link Window} definition to the ActiveWindow Operator.
+     * For example {@link SlidingWindow} or {@link TumblingWindow}
+     *
+     * @param window the new window definition
+     */
+    public KeyedScottyWindowOperator addWindow(Window window) {
+        windows.add(window);
+        return this;
+    }
+}
