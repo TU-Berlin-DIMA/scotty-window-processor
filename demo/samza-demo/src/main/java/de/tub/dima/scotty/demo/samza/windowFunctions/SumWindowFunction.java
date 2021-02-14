e67031a9e3e949dd318f6049e32d48340ee3844c
julianev <juliane.verwiebe@jevernet.de>
2021-02-14 19:35:23 +0100

    resolves https://github.com/TU-Berlin-DIMA/scotty-window-processor/issues/17

---
+++ demo/samza-demo/src/main/java/de/tub/dima/scotty/demo/samza/windowFunctions/SumWindowFunction.java
@@ -0,0 +1,15 @@
+package de.tub.dima.scotty.demo.samza.windowFunctions;
+
+import de.tub.dima.scotty.core.windowFunction.InvertibleReduceAggregateFunction;
+
+public class SumWindowFunction implements InvertibleReduceAggregateFunction<Integer> {
+    @Override
+    public Integer invert(Integer currentAggregate, Integer toRemove) {
+        return currentAggregate-toRemove;
+    }
+
+    @Override
+    public Integer combine(Integer partialAggregate1, Integer partialAggregate2) {
+        return partialAggregate1+partialAggregate2;
+    }
+}
