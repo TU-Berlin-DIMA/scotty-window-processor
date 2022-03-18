376ca0be605cf0efee2e1a7afdd8e3d731863876
Juliane Verwiebe <53897863+julianev@users.noreply.github.com>
2022-03-18 11:08:27 +0100

    Punctuation Window implementation and test (#28)
    * Punctuation Window Implementation and Test
    
    * PunctuationWindow Slicing Adaption
    
    * revisit Punctuation Window Implementation
    
    * remove   added logging dependency
    
    * add FlinkPunctuationDemo
    
    * Remove empty line
    
    * move FlinkDemo to demo folder
    
    * remove old shift tuples logic
    
    * remove split variable

---
+++ demo/flink-demo/src/main/java/de/tub/dima/scotty/demo/flink/FlinkPunctuationDemo.java
@@ -0,0 +1,41 @@
+package de.tub.dima.scotty.demo.flink;
+
+import de.tub.dima.scotty.core.windowType.PunctuationWindow;
+import de.tub.dima.scotty.demo.flink.windowFunctions.SumWindowFunction;
+import de.tub.dima.scotty.flinkconnector.KeyedScottyWindowOperator;
+import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.LocalStreamEnvironment;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.io.Serializable;
+
+public class FlinkPunctuationDemo  implements Serializable  {
+    public static void main(String[] args) throws Exception {
+
+        LocalStreamEnvironment sev = StreamExecutionEnvironment.createLocalEnvironment();
+        sev.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        sev.setParallelism(1);
+        sev.setMaxParallelism(1);
+
+        DataStream<Tuple2<Integer, Integer>> stream = sev.addSource(new DemoSource());
+
+        KeyedScottyWindowOperator<Tuple, Tuple2<Integer, Integer>, Tuple2<Integer, Integer>> processingFunction =
+                new KeyedScottyWindowOperator<>(new SumWindowFunction());
+
+        //define the punctuation, can have different values
+        Tuple2 p = new Tuple2(1, 9); //every time a tuple with the value 9 is emitted, a new window starts
+
+        processingFunction
+                .addWindow(new PunctuationWindow(p));
+
+        stream
+                .keyBy(0)
+                .process(processingFunction)
+                .print();
+
+        sev.execute("demo");
+    }
+}
